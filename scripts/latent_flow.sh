ab706c96be8a0f3210e04653efe3591e78006abe
Yue Song <songyue19960927@gmail.com>
2023-09-11 09:23:31 +0200

    Create latent_flow.sh

---
+++ scripts/latent_flow.sh
@@ -0,0 +1,23 @@
+pool="VAE_MNIST_VIS"
+eps=1
+shift_steps=16
+shift_leap=1
+# =====================
+
+declare -a EXPERIMENTS=("experiments/wip/VAE_MNIST-scratch-K3-D16-LearnGammas-eps0.0_8.1")
+
+python sample_vae.py --num-samples 10 --pool "VAE_MNIST_VIS" -g "VAE_MNIST" \
+
+wait
+
+for exp in "${EXPERIMENTS[@]}"
+do
+  # Traverse latent space
+  python traverse_latent_trunc_vae.py -v --gif \
+                                  --exp="${exp}" \
+                                  --pool=${pool} \
+                                  --eps=${eps} \
+                                  --shift-steps=${shift_steps} \
+                                  --shift-leap=${shift_leap} 
+                                
+done
