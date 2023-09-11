b55f473906b542ccaf5bd4539dafef91ee7afc88
Yue Song <songyue19960927@gmail.com>
2023-09-11 09:25:44 +0200

    Create isaac.sh

---
+++ scripts/isaac.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+isaac=true
+num_support_sets=8
+num_timesteps=8
+batch_size=128
+max_iter=100000
+tensorboard=true
+
+tb=""
+if $tensorboard ; then
+  tb="--tensorboard"
+fi
+
+python train_falcol_isaac.py $tb \
+                --num-support-sets=${num_support_sets} \
+                --num-timesteps=${num_timesteps} \
+                --batch-size=${batch_size} \
+                --max-iter=${max_iter} \
+                --log-freq=10 \
+                --ckp-freq=100 \
+                --isaac=${isaac}
