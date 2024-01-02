bbd5d89b704e705d1dc902ce250a9983dd741835
TobiasSchmidtDE <tsbaunatal@gmail.com>
2024-01-02 13:01:54 +0100

    add gitignore

--- lib/__init__.py
+++
@@ -1,7 +0,0 @@
-from .aux import create_exp_dir, update_stdout, update_progress, sample_z, create_summarizing_gif
-from .DiffPDE import DiffPDE
-from .HJPDE import HJPDE
-from .trainer_ot_scratch import TrainerOTScratch
-from .trainer_falcol_issac_scratch import TrainerFalcolIsaacScratch
-from .trainer_ot_scratch_weakly import TrainerOTScratchWeakly
-from .trainer_ot_scratch_weakly_shapes import TrainerOTScratchWeaklyShapes
